e9765c2df074c9dd0ca7a61aa78034dce159723c
marencoalf <alfredomarenco@boletea.com>
2021-02-28 13:58:32 -0600

    Creamos las funciones para poder hacer que se impriman tickets segun el caso para Tickets cancelados y para el corte de caja

--- resources/views/reports/show.blade.php
+++ resources/views/reports/show.blade.php
@@ -11,7 +11,8 @@
                 @switch($ticket->pagado)
                     @case(0)
                     <div class="flex justify-center bg-orange p-3">
-                        <h2 class="text-xl text-gray-100 font-bold">Orden: {{ $ticket->id }} | Estatus: No completado | Creada el {{ $ticket->created_at }}
+                        <h2 class="text-xl text-gray-100 font-bold">Orden: {{ $ticket->id }} | Estatus: No completado |
+                            Creada el {{ $ticket->created_at }}
                         </h2>
                     </div>
                     <div class="grid grid-cols-4 gap-3 justify-items-center items-center">
@@ -19,14 +20,15 @@
                         <div class="bg-gray-800 w-full text-center text-white font-bold rounded-t-md mt-4">Total</div>
                         <div class="bg-gray-800 w-full text-center text-white font-bold rounded-t-md mt-4">Fecha de
                             Ingreso</div>
-                        <div class="bg-gray-800 w-full text-center text-white font-bold rounded-t-md mt-4">Tiempo desde el ingreso
+                        <div class="bg-gray-800 w-full text-center text-white font-bold rounded-t-md mt-4">Tiempo desde el
+                            ingreso
                         </div>
                         <div class="my-6">{{ $ticket->plate }}</div>
                         <div class="my-6">$ {{ number_format($ticket->amount, 2) }}</div>
                         <div class="my-6">{{ $ticket->datetime_start }}</div>
                         <div class="my-6">{{ Carbon\Carbon::parse($ticket->datetime_start)->diffForHumans() }}</div>
                         <div class="col-span-4 text-center my-2 bg-gray-200 w-full">
-                            <small>Creada por {{ $ticket->user->name  }}</small>
+                            <small>Creada por {{ $ticket->user->name }}</small>
                         </div>
                         <div class="col-span-4 my-2">
                             <form action="{{ route('ticket.cancel', $ticket) }}" method="POST">
@@ -88,7 +90,8 @@
                     @break
                     @case(3)
                     <div class="flex justify-center bg-gray-600 p-3">
-                        <h2 class="text-xl text-gray-100 font-bold">Orden: {{ $ticket->id }} / Estatus: Cancelada fuera de tiempo
+                        <h2 class="text-xl text-gray-100 font-bold">Orden: {{ $ticket->id }} / Estatus: Cancelada fuera
+                            de tiempo
                         </h2>
                     </div>
                     <div class="grid grid-cols-4 gap-3 justify-items-center items-center">
@@ -122,6 +125,42 @@
                         <div class="my-6">$ {{ number_format($ticket->amount, 2) }}</div>
                         <div class="my-6">{{ $ticket->datetime_start }}</div>
                         <div class="my-6">{{ $ticket->datetime_end }}</div>
+                    </div>
+                    @break
+                    @case(5)
+                    <div class="flex justify-center bg-green-400 p-3">
+                        <h2 class="text-xl text-white  font-bold">Orden: {{ $ticket->id }} / Estatus: Completado -
+                            Carwash /
+                            Total: $ {{ number_format($ticket->amount, 2) }} </h2>
+                    </div>
+                    <div class="grid grid-cols-4 gap-3 justify-items-center items-center">
+                        <div class="bg-gray-800 w-full text-center text-white font-bold rounded-t-md mt-4">Placa</div>
+                        <div class="bg-gray-800 w-full text-center text-white font-bold rounded-t-md mt-4">Total</div>
+                        <div class="bg-gray-800 w-full text-center text-white font-bold rounded-t-md mt-4">Fecha de
+                            Ingreso</div>
+                        <div class="bg-gray-800 w-full text-center text-white font-bold rounded-t-md mt-4">Fecha de
+                            Salida
+                        </div>
+                        <div class="my-6">{{ $ticket->plate }}</div>
+                        <div class="my-6">$ {{ number_format($ticket->amount, 2) }}</div>
+                        <div class="my-6">{{ $ticket->datetime_start }}</div>
+                        <div class="my-6">{{ $ticket->datetime_end }}</div>
+                        <div class=" flex col-span-4 my-2">
+                            <form action="{{ route('printer.reprint', $ticket) }}" method="POST">
+                                @method('POST')
+                                @csrf
+                                <button type="submit"
+                                    class="mx-2 bg-gray-600 p-1 rounded-md text-center text-white font-semibold">Re-imprimir
+                                    comprobante</button>
+                            </form>
+                            <form action="{{ route('ticket.cancel', $ticket) }}" method="POST">
+                                @method('PUT')
+                                @csrf
+                                <button type="submit"
+                                    class="mx-2 bg-red-600 p-1 rounded-md text-center text-white font-semibold">Cancelar
+                                    Ticket</button>
+                            </form>
+                        </div>
                     </div>
                     @break
                     @default
